86b1097223fe99944cea35b75c9f7d3d454c96cf
Steven Leija <stevenleija@gmail.com>
2019-06-12 21:55:41 -0500

    Updates to get project to build

--- src/app/demo.module.ts
+++ src/app/demo.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core'
-import { DemoComponent } from './demo.component'
-import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { Angular2ImageGalleryModule } from 'angular2-image-gallery'
-import { HttpClientModule } from '@angular/common/http'
-import { Ng5SliderModule } from 'ng5-slider';
+import { NgModule } from "@angular/core";
+import { DemoComponent } from "./demo.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { Angular2ImageGalleryModule } from "angular2-image-gallery";
+import { HttpClientModule } from "@angular/common/http";
+import { Ng5SliderModule } from "ng5-slider";
 
 @NgModule({
-  declarations: [
-    DemoComponent
-  ],
+  declarations: [DemoComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -19,13 +17,8 @@ import { Ng5SliderModule } from 'ng5-slider';
     Angular2ImageGalleryModule,
     Ng5SliderModule
   ],
-  providers: [
-  ],
-  bootstrap: [
-    DemoComponent
-  ],
-  exports: [
-  ]
+  providers: [],
+  bootstrap: [DemoComponent],
+  exports: []
 })
-export class GalleryDemoModule {
-}
+export class GalleryDemoModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import {environment} from './environments/environment';
-import {GalleryDemoModule} from './app/demo.module';
+import { environment } from "./environments/environment";
+import { GalleryDemoModule } from "./app/demo.module";
 
-import 'hammerjs';
+import "hammerjs";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(GalleryDemoModule)
+platformBrowserDynamic()
+  .bootstrapModule(GalleryDemoModule)
   .catch(err => console.log(err));
